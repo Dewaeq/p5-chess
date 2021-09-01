1832a4ff1dd4bf7abb4283b6ab3293157cf68565
Dewaeq <54279351+Dewaeq@users.noreply.github.com>
2021-09-01 16:40:02 +0200

    book generation bug fixes

--- src/other/book/book_parser.py
+++ src/other/book/book_parser.py
@@ -29,40 +29,42 @@ def add_games_to_source_map():
         with open(f) as sourcefile:
             data += sourcefile.read()
 
-    with open("./games-parsed.pgn", "w") as destfile:
+    with open("./games-source.pgn", "w") as destfile:
         destfile.write(data)
 
+def create_book():
+    with open("./games-source.pgn") as book:
+        output = ""
+        source = remove_comments(book.read())
+        source = source.replace(". ", ".")
+        lines = source.splitlines()
 
-add_games_to_source_map()
+        for line in lines:
+            if "[" in line or line.isspace():
+                continue
+            line = line.replace("+", "")
+            line = line.replace("#", "")
+            line = line.replace("\n", "")
+            line = line.lstrip()
 
-with open("./games-source.pgn") as book:
-    output = ""
-    source = remove_comments(book.read())
-    source = source.replace(". ", ".")
-    lines = source.splitlines()
+            moves = line.split(" ")
+            for move in moves:
+                if "." in move:
+                    new_move = " " + move.split(".")[-1] + " "
+                    line = line.replace(move, new_move)
 
-    for line in lines:
-        if "[" in line or line.isspace():
-            continue
-        line = line.replace("+", "")
-        line = line.replace("#", "")
-        line = line.replace("\n", "")
-        line = line.lstrip()
+            if line.endswith("1/2-1/2") or line.endswith("0-1") or line.endswith("1-0"):
+                line += "\n"
 
-        moves = line.split(" ")
-        for move in moves:
-            if "." in move:
-                new_move = " " + move.split(".")[-1] + " "
-                line = line.replace(move, new_move)
+            output += line
 
-        if line.endswith("1/2-1/2") or line.endswith("0-1") or line.endswith("1-0"):
-            line += "\n"
+        output = output.replace("  ", " ")
+        output = output.replace("  ", " ")
 
-        output += line
+        with open("./games-parsed.pgn", "w") as output_book:
+            result = set([x.lstrip() for x in output.splitlines()])
+            output_book.write("\n".join(result))
 
-    output = output.replace("  ", " ")
-    output = output.replace("  ", " ")
-
-    with open("./games-parsed.pgn", "w") as output_book:
-        result = set([x.lstrip() for x in output.splitlines()])
-        output_book.write("\n".join(result))
+if __name__ == "__main__":
+    add_games_to_source_map()
+    create_book()
