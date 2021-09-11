3e698a8552261865e5de4c43ea5d090eaf40ea46
Dewaeq <54279351+Dewaeq@users.noreply.github.com>
2021-09-11 09:50:45 +0200

    proper multithreading with multiprocessing library

--- src/other/book/book_parser.py
+++ src/other/book/book_parser.py
@@ -2,7 +2,7 @@ import glob
 import os
 import sys
 import time
-from threading import Thread
+from multiprocessing import Pool as ThreadPool
 
 
 def remove_comments(data: str):
@@ -12,8 +12,11 @@ def remove_comments(data: str):
     for char in data:
         if char == "{":
             is_comment = True
+            continue
         elif char == "[":
             is_game_info = True
+            continue
+
         if not is_comment and not is_game_info:
             new_data += char
 
@@ -22,86 +25,80 @@ def remove_comments(data: str):
         elif char == "]":
             is_game_info = False
 
+    new_data = new_data.replace("\n", " ")
+    new_data = new_data.replace("`", "")
+    new_data = new_data.replace("1-0", "END\n")
+    new_data = new_data.replace("0-1", "END\n")
+    new_data = new_data.replace("0-1", "END\n")
+    new_data = new_data.replace("1/2-1/2`", "END\n")
+    new_data = new_data.replace("1/2-1/2", "END\n")
+    new_data = new_data.replace("*", "END\n")
+
     new_data = new_data.replace(". ", ".")
     new_data = new_data.replace("...", "")
+    new_data = new_data.replace("  ", " ")
+    new_data = new_data.replace("  ", " ")
+    new_data = new_data.replace("  ", " ")
 
     return new_data
 
 
+def open_and_parse_file(file_dir):
+    start = time.time()
+    with open(file_dir) as sourcefile:
+        data = remove_comments(sourcefile.read())
+        data = create_book(data)
+        end = time.time()
+        print(f"Parsed ({end - start:.2f}s): {file_dir}")
+        return data
+
+
 def add_games_to_source_map(files_dir):
-    data = ""
 
-    def open_and_parse_file(file_dir):
-        with open(file_dir) as sourcefile:
-            nonlocal data
-            data += remove_comments(sourcefile.read())
+    files = glob.glob(files_dir + "/*.pgn")
+    pool = ThreadPool(os.cpu_count() - 4)
+    files = [x for x in files if not "games-source.pgn" in x]
+    results = pool.map(open_and_parse_file, files)
+    results = "".join(results).splitlines()
+    results = sorted(set(results), key=str.lower)
 
-    sourcefiles = glob.glob(files_dir + "/*.pgn")
-    threads = []
-
-    for f in sourcefiles:
-        if "games-source.pgn" in f or "games-parsed.pgn" in f:
-            continue
-        thread = Thread(target=open_and_parse_file, args=(f, ))
-        threads.append(thread)
-
-    print(f"{len(threads)} threads are running")
-
-    for thread in threads:
-        thread.start()
-        thread.join()
-
-    total_size = 0
     with open(files_dir + "/games-source.pgn", "w") as destfile:
-        destfile.write(data)
-    
-    total_size = os.path.getsize(files_dir + "/games-source.pgn") / (1024 ** 2)
-
-    print(f"Wrote {total_size:.2f} MB to {files_dir}/games-source.pgn")
+        destfile.write("\n".join(results))
 
 
-def create_book(files_dir):
-    with open(files_dir + "/games-source.pgn") as book:
-        output = ""
-        source = book.read()
-        lines = source.splitlines()
+def create_book(data: str):
+    output = ""
+    lines = data.splitlines()
 
-        for line in lines:
-            if "[" in line or line.isspace():
-                continue
-            line = line.replace("+", "")
-            line = line.replace("#", "")
-            line = line.replace("\n", "")
-            line = line.lstrip()
+    for line in lines:
+        if "[" in line or line.isspace():
+            continue
+        line = line.replace("+", "")
+        line = line.replace("#", "")
 
-            moves = line.split(" ")
-            for move in moves:
-                if "." in move:
-                    new_move = " " + move.split(".")[-1] + " "
-                    line = line.replace(move, new_move)
+        moves = line.split(" ")
+        for move in moves:
+            if "." in move:
+                new_move = " " + move.split(".")[-1] + " "
+                line = line.replace(move, new_move)
 
-            if line.endswith("1/2-1/2") or line.endswith("0-1") or line.endswith("1-0"):
-                line += "\n"
+        output += line.lstrip() + "\n"
 
-            output += line
-
-        output = output.replace("  ", " ")
-        output = output.replace("  ", " ")
-
-        with open(files_dir + "/games-parsed.pgn", "w") as output_book:
-            print(f"Total num of parsed games: {len(output.splitlines())}")
-            result = set([x.lstrip() for x in output.splitlines()])
-            result = sorted(result, key=str.lower)
-            output_book.write("\n".join(result))
-
-            print(f"Wrote {len(result)} games to {files_dir}/games-parsed.pgn")
+    output = output.replace("  ", " ")
+    output = output.replace("  ", " ")
+    return output
 
 
 if __name__ == "__main__":
     start = time.time()
     files_dir = sys.argv[1]
+
     add_games_to_source_map(files_dir)
-    create_book(files_dir)
+
+    total_size = os.path.getsize(files_dir + "/games-source.pgn") / (1024 ** 2)
+
+    print(f"Wrote {total_size:.2f} MB to {files_dir}/games-source.pgn")
+
     end = time.time()
 
     print(f"Operations took {end - start:.2f} seconds")
